6f2179444547b567d6f803714bf9ee23dcb90df0
Lyyyyb <22320219@hdu.edu.cn>
2024-02-20 17:09:31 +0800

    gengxin

---
+++ build/autopaintrobot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyb/AutoPaintRobot/src/autopaintrobot
+# Build directory: /home/lyb/AutoPaintRobot/build/autopaintrobot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
