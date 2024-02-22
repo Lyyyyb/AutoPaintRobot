7708d0da6df67e89d040056c594ee78e436dec2d
Yibo Liang <22320219@hdu.edu.cn>
2024-02-22 14:23:37 +0800

    gengxin

---
+++ src/robot_bringup/include/robot_bringup/mbot_linux_serial.h
@@ -0,0 +1,21 @@
+#ifndef MBOT_LINUX_SERIAL_H
+#define MBOT_LINUX_SERIAL_H
+
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/asio.hpp>
+#include <geometry_msgs/Twist.h>
+
+extern void serialInit();
+extern void writeSpeed(double RobotV, double YawRate,unsigned char ctrlFlag);
+extern bool readSpeed(double &vx,double &vth,double &th,unsigned char &ctrlFlag);
+unsigned char getCrc8(unsigned char *ptr, unsigned short len);
+
+
+//extern boost::asio::io_service iosev;
+//extern boost::asio::serial_port sp(iosev, "/dev/ttyUSB0");
+//extern boost::system::error_code err;
+#endif
