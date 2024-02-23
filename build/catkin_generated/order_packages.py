7668cea8869ad025b96e1ef3b8a165a134a57644
Lyyyyb <22320219@hdu.edu.cn>
2024-02-23 14:49:37 +0800

    Remove build and devel folders from tracking

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/lyb/AutoPaintRobot/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lyb/catkin_ws/devel;/home/lyb/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/lyb/catkin_ws/devel;/home/lyb/ros_ws/devel;/opt/ros/noetic' != '' else []
