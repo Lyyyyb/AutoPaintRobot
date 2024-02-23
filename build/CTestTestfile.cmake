0b97b75c839aaac01bb0b0b1b5b89f4fd27b4e5a
Lyyyyb <22320219@hdu.edu.cn>
2024-02-23 14:46:04 +0800

    Remove build and devel folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lyb/AutoPaintRobot/src
-# Build directory: /home/lyb/AutoPaintRobot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("autopaintrobot")
--- build/autopaintrobot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lyb/AutoPaintRobot/src/autopaintrobot
-# Build directory: /home/lyb/AutoPaintRobot/build/autopaintrobot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
